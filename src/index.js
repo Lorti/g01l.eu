5d42882c38046b767746c6d1fe69d5ed4aabf2aa
Manuel Wieser <office@manuelwieser.com>
2016-10-16 17:12:35 +0200

    cleanup

--- src/index.js
+++ src/index.js
@@ -1,42 +1,25 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { createStore } from 'redux';
-//import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import { Router, Route, browserHistory } from 'react-router';
-//import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
+import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
 
 import App from './containers/App';
-//import reducer from './reducers';
-import style from './reducers/style';
+import reducer from './reducers/style';
 
-
-
-import 'normalize.css'; // Tell Webpack that Button.js uses these styles
-
+import 'normalize.css';
 
 const logger = store => next => action => {
-//    console.group(action.type);
-//    console.info('dispatching', action);
-//    let result = next(action);
-//    console.log('next state', store.getState());
-
-//    const state = store.getState();
-//    browserHistory.replace('/' + state.textString + '/' + state.strokeWidth);
-
-//    console.groupEnd(action.type);
-//    return result;
-
+    if (action.type === 'CHANGE_TEXT_STRING') {
+        document.title = action.value;
+    }
     return next(action);
 };
 
-
-//const store = createStore(reducer);
-//const store = createStore(style, composeWithDevTools(
-//    applyMiddleware(logger)
-//));
-
-const store = createStore(style, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const store = createStore(reducer, composeWithDevTools(
+    applyMiddleware(logger)
+));
 
 render(
     <Provider store={store}>
@@ -48,5 +31,3 @@ render(
     </Provider>,
     document.getElementById('root')
 );
-
-
